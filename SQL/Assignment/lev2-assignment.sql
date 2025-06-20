855c8abc88b6563958d452b99c5e3cc38494d777
Shantanu Kadam <shantanuekadam@gmail.com>
2025-06-21 00:56:10 +0530

    uploaded the assignment

---
+++ SQL/Assignment/lev2-assignment.sql
@@ -0,0 +1,25 @@
+
+-- ASIGMENTS FROM LECTURE 2 -- 
+-- 1. Get the first name and email of all employees in department ID 20.
+
+-- 2. List all departments that have a manager ID greater than 102.
+
+-- 3. Find employees hired after January 1, 2014. Show their first name, last name, and hired date.
+
+-- 4. Display all job titles that have a max salary above 80,000.
+
+-- 5. Find the employees who earn a salary between 50,000 and 70,000 (inclusive).
+
+-- 6. List employees whose phone number starts with '23'.
+
+-- 7. Get all distinct departments employees belong to, sorted in descending order.
+
+-- 8. Find all employees who are assigned to department ID 10 and have a job ID 'J103'.
+
+-- 9. Find salaries where reducing 2,000 still keeps it above 50,000. Show salary ID, original and adjusted amount.
+
+-- 10. Get a list of salaries sorted by amount in descending order, and show only the top 3 salary only.
+
+-- 11. Find all employees id who have been assigned to any project (i.e., exist in assignments table).
+
+-- 12. Insert a new row in the attendance table for employee ID 103 for today's date with check-in as 09:00 and check-out as 17:00 and log_id as 10.
