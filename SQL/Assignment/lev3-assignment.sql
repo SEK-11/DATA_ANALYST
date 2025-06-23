b3a7ff47b7c292ae28ba22cd9493b36557646217
Shantanu Kadam <shantanuekadam@gmail.com>
2025-06-24 00:16:27 +0530

    assignment

---
+++ SQL/Assignment/lev3-assignment.sql
@@ -0,0 +1,23 @@
+-- Employees and their salaries + departments using multi-joins [use difference types of join and compare the results]
+
+
+-- Employees not assigned to any project (Without using join)
+
+
+-- Departments with average salary > 50,000 and at least 1 project
+
+
+-- Get the top 3 most assigned projects by number of employees
+
+
+-- Show employees assigned to projects outside their own department
+
+
+-- List employees with their number of assigned projects and most recent hire date
+
+
+-- For each project, show the total employees assigned and the latest start date
+
+
+-- Department-wise count of unique projects employees are assigned to
+
